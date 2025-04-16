28c2ff4d669c0fd8af5df8472aa01bf3f216116d
VrmVrmCar <adamwhittome01@gmail.com>
2025-04-16 13:45:05 +0800

    Dilution Factor Implemented before Loading

---
+++ Assets/Script/Analysis.cs
@@ -0,0 +1,111 @@
+using UnityEngine;
+using System.IO;
+using System.Linq;
+
+public class Analysis
+{
+    public string folderPath;
+
+    public Analysis(string folderPath)
+    {
+        this.folderPath = folderPath;
+    } 
+
+    public string foundTimeSlice()
+    {
+        string resultString = "";
+        if (validFolder())
+        {
+
+            resultString += $"File Count (Time Slices): {findTimeSlices()}";
+
+        }
+        return resultString;
+    }
+
+    public string foundCoordinate()
+    {
+        string resultString = "";
+        if (validFolder())
+        {
+
+            resultString += $"Rows per File (Coordinates): {findDataAmount()}";
+
+        }
+        return resultString;
+    }
+
+    public string foundDetails()
+    {
+        string resultString = "";
+        if (validFolder())
+        {
+            
+            resultString += $"File Count (Time Slices): {findTimeSlices()} \n \n";
+
+            resultString += $"Rows per File (Coordinates): {findDataAmount()} \n";
+        }
+        return resultString;
+    }
+
+    public string foundStatus()
+    {
+        string returnString = "";
+        if (!validFolder())
+        {
+            returnString = "Failure to find folder";
+        } else
+        {
+            returnString = "Successfully found folder";
+        }
+        return returnString;
+    }
+
+
+    public bool validFolder()
+    {
+        return Directory.Exists(folderPath);
+    }
+
+    public int findTimeSlices()
+    {
+        int timestep = 0;
+        int count = 0;
+
+        while (true)
+        {
+            string fileName = $"All_mesh_data_at_timestep_{timestep}.csv";
+            string fullPath = Path.Combine(folderPath, fileName);
+
+
+            if (File.Exists(fullPath))
+            {
+                count++;
+            }
+            timestep++;
+            if (timestep > 101) break;
+
+        }
+        return count;
+    }
+
+    public int findDataAmount()
+    {
+        int count = 0;
+        string filePath = folderPath + "/All_mesh_data_at_timestep_0.csv";
+        using (var reader = new StreamReader(filePath))
+        {
+            reader.ReadLine();
+            reader.ReadLine();
+            while (reader.EndOfStream == false)
+            {
+                reader.ReadLine();
+                count++;
+                
+            }
+        }
+        return count-2;
+    }
+
+
+}
--- Assets/Script/Main Loader.cs
+++ Assets/Script/Main Loader.cs
@@ -7,13 +7,23 @@ using System.Linq;
 
 public class MainLoader : MonoBehaviour
 {
+    public int timeSliceDilation = 1;
+    public int coordinateDilation = 1;
+    public TMPro.TMP_Text timeSliceDilationDisplay;
+    public TMPro.TMP_Text coordinateDilationDisplay;
+
+
     public TMPro.TMP_InputField fileStructure;
+    public TMPro.TMP_Text foundTimeSlice;
+    public TMPro.TMP_Text foundCoordinate;
+    public TMPro.TMP_Text foundStatus;
+    public Button LoadButton;
     public string csvPath;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        LoadButton.interactable = false;
     }
 
     // Update is called once per frame
@@ -22,11 +32,67 @@ public class MainLoader : MonoBehaviour
         
     }
 
-    public void SubmitButton()
+    public void TimeSliceDilationUp()
+    {
+        if (!(timeSliceDilation == 10)) //code upper bound
+        {
+            timeSliceDilation++;
+            timeSliceDilationDisplay.text = timeSliceDilation.ToString();
+        }
+    }
+
+    public void TimeSliceDilationDown()
+    {
+        if (!(timeSliceDilation == 1)) 
+        {
+            timeSliceDilation--;
+            timeSliceDilationDisplay.text = timeSliceDilation.ToString();
+        }
+
+    }
+
+    public void CoordinateDilationUp()
+    {
+        if (!(coordinateDilation == 10))
+        {
+            coordinateDilation++;
+            coordinateDilationDisplay.text = coordinateDilation.ToString();
+        }
+    }
+
+    public void CoordinateDilationDown()
+    {
+        if (!(coordinateDilation == 1))
+        {
+            coordinateDilation--;
+            coordinateDilationDisplay.text = coordinateDilation.ToString();
+        }
+    }
+
+    public void FindButton()
+    {
+        Analysis myAnalysis = new Analysis(fileStructure.text);
+        foundTimeSlice.text = myAnalysis.foundTimeSlice();
+        foundCoordinate.text = myAnalysis.foundCoordinate();
+        foundStatus.text = myAnalysis.foundStatus();
+        if (myAnalysis.validFolder())
+        {
+            fileStructure.textComponent.color = Color.green;
+            LoadButton.interactable = true;
+
+        }
+        else
+        {
+            fileStructure.textComponent.color = Color.red;
+            LoadButton.interactable = false;
+        }   
+    }
+
+    public void LoadButtonClick()
     {
         Collision myCollision = new Collision();
         csvPath = fileStructure.text;
-        myCollision.Loader(csvPath);
+        myCollision.Loader(csvPath, timeSliceDilation, coordinateDilation);
         TestCollisionData(myCollision);
     }
 
